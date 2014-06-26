aac7bc1398544668d184b8045cb46ec479c18dc6
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-26 12:58:18 +0700

    prevent concurrent login

--- authorization-server/src/main/java/com/muhardin/endy/belajar/springoauth2/authserver/SecurityConfig.java
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/springoauth2/authserver/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(true);
+        
         http.authorizeRequests()
                 .antMatchers("/login.jsp").permitAll()
                 .and()
